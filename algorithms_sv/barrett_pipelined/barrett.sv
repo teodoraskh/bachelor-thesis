a185fb0d5617ef14bfd71e78cf4e09eb1867aecc
Teodora Alexandrescu <t.alexandrescu@student.tugraz.at>
2025-04-29 14:43:48 +0200

    fix signal usage in barrett

--- algorithms_sv/barrett_pipelined/barrett.sv
+++ algorithms_sv/barrett_pipelined/barrett.sv
@@ -12,35 +12,27 @@ module barrett_pipelined (
   output logic                    valid_o    // Result valid flag
 );
 
-// PRECOMP, APPROX, 
-typedef enum logic[2:0] {LOAD, PRECOMP, APPROX, REDUCE, FINISH} state_t;
+typedef enum logic[2:0] {LOAD, APPROX, REDUCE, FINISH} state_t;
 
 state_t curr_state, next_state;
 
 // 1, start_mult2, 
-logic start_mult, m_finish, a_finish;
+logic m_finish, a_finish;
 logic d_finish;
 logic [63:0] x_delayed;
 
-// TODO: but is this really necessary in this case?
 shiftreg #(
-  //4, because we only have 4 pipeline stages without intermediate steps for Barrett reduction
-  // might have to change it to NUM_MULS + 2 + 4 when we use pipelined multiplication too
-  // edit: it's not necessary to stall start_i by additional cycles due to multiplier, because multiplier is internally synchronized
-  // additionally, the multiplier's finish_o signal is used to start with the next barrett pipeline stage
-  // (NUM_MULS + 2) * 2*4 + 
-    .SHIFT((NUM_MULS + 2) * 2 + 4), //4, because we only have 4 pipeline stages without intermediate steps
-    .DATA(1) // sets the width of the data being shifted (1 bit wide)
-) shift_finish ( // HINT: this is the name of the module
-    // these are port connections:
+    .SHIFT((NUM_MULS + 2) * 2 + 1), 
+    .DATA(1)
+) shift_finish (
     .clk_i(clk_i),
     .data_i(start_i),
     .data_o(d_finish)
 );
 
 shiftreg #(
-    .SHIFT((NUM_MULS + 2) * 2 + 3), //4, because we only have 4 pipeline stages without intermediate steps
-    .DATA(64) // sets the width of the data being shifted (1 bit wide)
+    .SHIFT((NUM_MULS + 2) * 2),
+    .DATA(64)
 ) shift_in (
     .clk_i(clk_i),
     .data_i(x_i),
@@ -90,15 +82,13 @@ end
 //             $time, curr_state.name(), x_i, x_delayed, busy_p_o, m_finish, a_finish, d_finish);
 // end
 
-always_ff @(posedge clk_i) begin
-    $display("Cycle: %d, State: %s, x_i: %h, x_delayed: %h, out: %h, valid: %b",
-            $time, curr_state.name(), x_i, x_delayed, result_o, valid_o);
-end
+// always_ff @(posedge clk_i) begin
+//     $display("Cycle: %d, State: %s, x_i: %h, x_delayed: %h, out: %h, valid: %b",
+//             $time, curr_state.name(), x_i, x_delayed, result_o, valid_o);
+// end
 
 logic busy_p_o;
 logic [127:0] xmu_precomp;
-logic [63:0] mult_a_input;
-logic [63:0] mult_b_input;
 multiplier_top multiplier_precomp(
   .clk_i(clk_i),              // Rising edge active clk.
   .rst_ni(rst_ni),            // Active low reset.
@@ -131,7 +121,7 @@ logic [63:0] tmp;
 
 always_comb begin
   result_next = result_o;
-  if (!busy_a_o) begin
+  if (a_finish) begin
     tmp = x_delayed - qm_result;
     result_next = (tmp < m_i) ? tmp : tmp - m_i;
   end
