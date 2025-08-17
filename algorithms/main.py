78d5fadd99dd651e8cbc6aef281aca07d46b9f68
Teodora Alexandrescu <t.alexandrescu@student.tugraz.at>
2025-08-17 15:07:31 +0200

    refactor testbench algorithms

--- algorithms/main.py
+++ algorithms/main.py
@@ -4,11 +4,6 @@ import time
 import sys
 from reductions.modred import ALGORITHMS
 
-# By using numpy, we can do everything with vectorized operations
-# This approach doesn't yet use NTT, as its sole purpose is to help
-# me understand how much slower the operations are without it.
-# Afterwards, when I have gotten the results, the NTT step will also be implemented
-
 # Use a seed for reproducible results!!!
 np.random.seed(7)
 
@@ -32,20 +27,12 @@ if __name__ == "__main__":
   for _ in range(0, 1):
     # Get the polynomials from a uniform distribution:
     degree = np.random.randint(2**8, 2**10)
-    
-    # modulus = 2450863333
-
-    # modulus = 7069
-    # modulus = 17
-
-    # force nodulus to be odd?
-    # similar approach here: https://www.nayuki.io/res/montgomery-reduction-algorithm/montgomery-reducer.py
-    modulus = np.random.randint(2, (1 << 63) - 1) | 1  
-    # these VVV aren't
-    # modulus = np.random.randint(2, (1 << 32) - 1) | 1
-    # modulus = 7
-    # this is montgomery friendly
-    # modulus = (1 << 62) - 1
+    # degree = 256
+    # modulus = np.random.randint(2, (1 << 63) - 1) | 1
+    # kyber modulus
+    # modulus = 3329
+    # dilithium modulus
+    modulus = 8380417
 
     # Polynomial coefficients between 0 and the modulus
     A = np.random.randint(0, modulus, size=degree, dtype=np.uint64)
@@ -67,51 +54,18 @@ if __name__ == "__main__":
       print(f"Modulus:   {modulus}")
       start_time = time.perf_counter()
       if algorithm == "montgomery":
-        # A_m = reduction_instance.to_montgomery(A)
-        # B_m = reduction_instance.to_montgomery(B)
-        # print(A_m)
-        # print(B_m)
-
-        # this VVVV will of course not work because convolution will make the
-        # coefficients larger than modulus^2 - and this input should be avoided 
-        # for barrett reduction, where 0 <= input < modulus^2
-        # conv = reduction_instance.to_montgomery(np.convolve(A, B))
-
-        # if we were to use NTT, then this convolution would become element-wise
-        # multiplication, which ensures the fact that:
-        # 1. the polynomial degrees do not exceed *n*
-        # 2. polymul in NTT form is now element-wise multiplication instead of convolution,
-        #    which ensures the fact that the result of the multiplied coefficients will not
-        #    exceed modulus^2.
-        # therefore, for the sake of simplicity, we'll just use element-wise multiplication,
-        # which will now fit the algorithms
-        # conv = reduction_instance.to_montgomery(A) * reduction_instance.to_montgomery(B)
-        print("not mont: ", np.vectorize(hex)(A*B))
-        conv = reduction_instance.to_montgomery(A*B)
-        # conv = np.convolve(A_m, B_m)
+        conv = A * reduction_instance.to_montgomery(B)
       else:
         conv = A * B
 
-        # print(A)
-        # print(B)
-
       print("conv:", np.vectorize(hex)(conv))
-      # print("conv:", conv)
-
-      # the polynomial reduction by x^n + 1 is missing as well for now.
-      # use np.convolve(A, B) instead of A * B which is element-wise multiplication
-      C = reduction_instance.reduce(conv) 
-      # if algorithm == "montgomery":
-      #   C = reduction_instance.reduce(C)
-
+      C = reduction_instance.reduce(conv)
       results[algorithm] = C
 
       end_time = time.perf_counter()
       print(f"Elapsed time: {(end_time - start_time) * 1000} (ms)")
       print(f"New polynomial degree: {C.shape[0]}")
       print(np.vectorize(hex)(C))
-      # print(C)
-      # print(C[60])
 
     reference_key, reference_value = next(iter(results.items()))
     mismatched_keys = [key for key, value in results.items() if not np.array_equal(value, reference_value)]
--- algorithms/reductions/modred.py
+++ algorithms/reductions/modred.py
@@ -9,10 +9,10 @@ class ModularReduction:
 
   def reduce(self, mult):
         raise NotImplementedError("Subclasses must implement the reduce method.")
-  
+
   @classmethod
   def available_algorithms(cls):
-        return {subclass.__name__.lower().replace("reduction", ""): subclass 
+        return {subclass.__name__.lower().replace("reduction", ""): subclass
                 for subclass in cls.__subclasses__()}
 
   def __repr__(self):
@@ -35,15 +35,11 @@ class BarrettReduction(ModularReduction):
     assert np.all(mult <= self.modulus**2)
     approximated_quotient = np.multiply(mult, self.mu, dtype=np.object_) >> np.multiply(2, self.k, dtype=np.object_)
     remainder = mult - np.multiply(approximated_quotient,self.modulus, dtype=np.object_)
-    # This will do a bitmask over the entire np.array() and will thus avoid any
-    # looping or branching
+
+    # This will do a bitmask over the entire np.array() and will thus avoid any branching
     remainder -= np.multiply(self.modulus, (remainder >= self.modulus), dtype=np.object_)
-    # if there are still elements in the remainder array that are greater than modulus, reduce them
-    # if np.any(remainder >= self.modulus):
-    #   remainder[remainder >= self.modulus] -= self.modulus
-    # remainder[remainder >= self.modulus] -= self.modulus
     return remainder
-  
+
 
 class MontgomeryReduction(ModularReduction):
     def __init__(self, modulus):
@@ -55,83 +51,49 @@ class MontgomeryReduction(ModularReduction):
       self.n_prime = np.object_(-pow(self.modulus, -1, self.radix))
 
     def to_montgomery(self, x):
-      # return np.object_(self.reduce(np.object_(x) * self.radix * self.radix, True))
       return np.object_(np.object_(x) * self.radix % self.modulus)
 
     def from_montgomery(self, x):
       return np.object_((np.object_(x) * self.inv_radix) % self.modulus)
-    
-    def find_modular_inverse(self, x):
-      t, new_t = 0, 1
-      r, new_r = self.radix, self.modulus
-
-      while new_r != 0:
-        quotient = r // new_r
-        t, new_t = new_t, t - quotient * new_t
-        r, new_r = new_r, r - quotient * new_r
-
-      if r > 1:
-        raise ValueError(f"{self.modulus} has no modular inverse modulo {self.radix}")
-      if t < 0:
-        t += self.radix
-      return -t
 
     def reduce(self, T):
-      # self.n_prime = self.find_modular_inverse(T)
-      print("correction: ", self.n_prime)
       m = np.object_((T & (self.radix - 1)) * self.n_prime & (self.radix - 1))
       t = np.object_((T + m * self.modulus) >> self.modulus.bit_length())
       t = np.where(t >= self.modulus, t - self.modulus, t)
       return t
 
-    # NOTE: this vvv also works fine
-    # def reduce(self, mult):
-    #   # assert np.all(mult <= self.modulus*2)
-    #   acc = np.object_(mult.copy())
-    #   # go through every bit of the modulus
-    #   for _ in range(self.modulus.bit_length()):
-    #     # get lsb of every coefficient found in acc, then do conditional reduction
-    #     LSB = np.object_(acc & 1)
-    #     acc = np.object_(np.where(LSB == 0, np.object_(acc >> 1), np.object_(np.object_(acc + self.modulus) >> 1)))
-    #   # acc = np.where(acc >= self.modulus, acc - self.modulus, acc)
-    #   return acc
 
 
-    
 
+# General Mersenne Reduction
 class ShiftAddReduction(ModularReduction):
     # If we use the Mersenne modulus property:
-    # m = 2^k -1 => 2^k eq. 1 (mod 2^k-1)
-    # we can split our input number into k-bit chunks
-    # the remainder is computed by summing up the k-bit chunks
+    # m = 2^k - n => 2^k congr. n (mod 2^k - n)
+    # we can split our input number into k-bit chunks and by
+    # folding the chunks into the range [0, M)
+    # the remainder is computed by summing up the folded k-bit chunks
     def __init__(self, modulus):
         super().__init__(modulus)
         self.k = self.modulus.bit_length()
-        self.k_2 = np.object_(1 << self.k)
+        self.k_2 = np.object_(1 << self.k)  # 2^k
         self.mask = np.object_(self.k_2 - 1)
-        self.coeff = np.object_(self.k_2 % self.modulus)
+        self.coeff = np.object_(self.k_2 % self.modulus)  # 2^k % modulus to get n.
 
-    # Find k, and also check if  modulus is a mersenne prime:
-    # if m = 7 => 7 = 2^k -1 => 8 = 2^k => k = log_2(8) => k = 3.
-    # if k is an integer, then m is mersenne
-    
+
     def reduce(self, mult):
       print(f"bitlength: {self.k}")
       print(f"correction: {self.coeff}")
       remainder = np.object_(mult.copy())
-      # while np.any(mult >= self.modulus):
-      for _ in range(self.modulus.bit_length()):
+      while np.any(remainder >= self.modulus):
         # mask gets least k bits, r >> k removes those k bits
-        # print(remainder)
-        # by default, mersenne use coeff = 1 because 2^k congr 1 mod modulus
-        # so if we find a way to easily precompute coeff...
         hi = np.object_(remainder >> self.k)
         lo = np.object_(remainder & self.mask)
+        # Fold anything above 2^k using n
         remainder = np.object_(self.coeff * hi  + lo)
       if np.any(remainder >= self.modulus):
          print("t")
          remainder[remainder>= self.modulus] -= self.modulus
       return remainder
 
-  
+
 ALGORITHMS = ModularReduction.available_algorithms()
